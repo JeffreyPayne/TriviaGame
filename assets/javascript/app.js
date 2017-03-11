a08bee852f6f367a0b7c5416c6e00dd52493a940
JeffreyPayne <jeffreypayne@jeffrey-paynes-macbook-pro.local>
2017-03-11 08:52:53 -0800

    files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,109 @@
+var panel = $("#quiz-area");
 
-var correctAnswers = 0;
+// Question set
+var questions = [{
+  question: "Which was the lost fourth member of Snap, Krackle, and Pop?",
+  answers: ["Ping", "Bam", "Pow", "Bloop"],
+  correctAnswer: "Pow"
+}, {
+  question: "What year was the Declaration of Independence ratified?",
+  answers: ["1768", "1772", "1776", "1781"],
+  correctAnswer: "1776"
+}, {
+  question: "Which NFL team has the most Super Bowl wins?",
+  answers: ["Pittsburg Steelers", "New England Patriots", "San Francisco 49ers", "Atlanta Falcons"],
+  correctAnswer: "Pittsburg Steelers"
+}];
 
-var incorrectAnswers = 0;
+// Variable that will hold the setInterval
+var timer;
 
-var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+var game = {
 
-function checkAnswers() {
-	// here is where we put the code that will execute, when the function is called
-	// 4. When timer runs out check to see what questions the player got right or wrong
-	alert("You got " + correctAnswers + "correct  and " + incorrectAnswers + "incorrect.");
+  correct: 0,
+  incorrect: 0,
+  counter: 30,
 
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.done();
+    }
+  },
 
-   // 5. Option to restart the game appears
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
 
-}
+    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>30</span> Seconds</h2>");
 
-// 1. Start timer
-$( document ).ready(function() {
-    //when document is ready, run the below code
-    setTimeout(checkAnswers(), 3000);
+    $("#start").remove();
+
+    for (var i = 0; i < questions.length; i++) {
+      panel.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        panel.append("<input type='radio' name='question-" + i +
+        "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+      }
+    }
+
+    panel.append("<button id='done'>Done</button>");
+  },
+
+  done: function() {
+
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() === questions[0].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-1']:checked"), function() {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() === questions[2].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+
+  },
+
+  result: function() {
+
+    clearInterval(timer);
+
+    $("#sub-wrapper h2").remove();
+
+    panel.html("<h2>All Done!</h2>");
+    panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  }
+};
+
+// CLICK EVENTS
+
+$(document).on("click", "#start", function() {
+  game.start();
 });
 
-// 2. Three multiple choice questions appear with three answer choices buttons
 
-
-// 3. Player goes through each question and attempts to answer each question in the alloted time
-// when player clicks an answer, update the variables to add one to the correct answers or the incorrect answers
-// this selects an element with id of "question one"
-$("#q-one-true").click(function() {
-	// when the html element with the target id is clicked, the below code will run
-	correctAnswers += 1;
+$(document).on("click", "#done", function() {
+  game.done();
 });
-
-$("#q-one-false").click(function() {
-	// when the html element with the target id is clicked, the below code will run
-	incorrectAnswers += 1;
-});
-
-
-
-
